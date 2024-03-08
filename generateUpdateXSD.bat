b5100196843d4739fdb9c2e672a86b4f7eee0d6d
supertomcat <bilderherunterlader@gmail.com>
2024-03-08 09:47:10 +0100

    Improved XSD

--- generateUpdateXSD.bat
+++ generateUpdateXSD.bat
@@ -1,1 +1,3 @@
-"%JAXB_HOME%\bin\xjc" -d src/generated/java -p ch.supertomcat.bh.update.xml src/main/resources/ch/supertomcat/updater/update/update.xsd
+"%JAXB_HOME%\bin\xjc" -d src/generated/java -p ch.supertomcat.bh.update.xml src/main/resources/ch/supertomcat/updater/update/update.xsd -b src/main/resources/ch/supertomcat/updater/update/update-binding.xjb
+
+::-no-header
